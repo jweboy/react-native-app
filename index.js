7587091e8f9cba51d7f43d63e8f7cfa18338e97d
jweboy <1259524459@qq.com>
2017-09-30 17:18:46 +0800

    add expo test

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+import Expo from 'expo'
+import SplashScreen from 'react-native-splash-screen'
+import HomePage from './src/pages/HomePage'
+
+console.warn(Expo);
+
+export default class myReactNativeApp extends Component {
+  componentDidMount = () => {
+    SplashScreen.hide()
+  }
+
+  render() {
+    return (
+      <HomePage />
+    );
+  }
+}
+
+// AppRegistry.registerComponent('myReactNativeApp', () => myReactNativeApp);
+Expo.registerRootComponent(myReactNativeApp)
