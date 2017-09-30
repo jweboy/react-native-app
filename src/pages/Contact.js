2b4a31311ed8b06449ee74419149824e3c4f5573
jweboy <1259524459@qq.com>
2017-09-30 10:05:50 +0800

    add FlatList Component with ContactScreen

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text, Image } from 'react-native'
+import { StyleSheet, View, Text, Image, FlatList, ActivityIndicator } from 'react-native'
+import { List, ListItem, SearchBar } from 'react-native-elements'
 
 export default class Contact extends Component {
+  state = {
+    loading: false,
+    data: [],
+    page: 1,
+    seed: 1,
+    error: null,
+    refreshing: false,
+  }
   static navigationOptions = ({ navigation }) => ({
     title: '联系人',
     headerStyle: {
@@ -22,10 +31,109 @@ export default class Contact extends Component {
       />
     )
   })
-  render() {
+  componentDidMount = () => {
+    this.getRemoteRequest()
+  }
+  getRemoteRequest = () => {
+    const { page, seed } = this.state
+    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`
+    this.setState({ loading: true })
+
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: [...this.state.data, ...res.results],
+          error: res.error || null,
+          loading: false,
+          refreshing: false,
+        })
+      })
+      .catch(error => {
+        this.setState({
+          error,
+          loading: false,
+          refreshing: false,
+        })
+      })
+  }
+  renderHeader = () => (
+    <SearchBar placeholder={'Type here ...'} round />
+  )
+  renderFooter = () => {
+    if (!this.state.loading) return null
+
     return (
-      <View style={styles.container}>
-        <Text>联系人界面</Text>
+      <View style={{
+        paddingVertical: 20,
+        borderTopWidth: 1,
+        borderColor: '#CED0CE',
+      }}>
+        <ActivityIndicator animating size={'large'} />
+      </View>
+    )
+  }
+  renderItem = ({ item }) => (
+    <ListItem
+      roundAvatar
+      title={`${item.name.first},${item.name.last}`}
+      subtitle={item.email}
+      avatar={{ uri: item.picture.thumbnail }}
+      containerStyle={{ borderBottomWidth: 0 }}
+    />
+  )
+  renderDefault = () => (
+    <View>
+      <Text>联系人界面</Text>
+    </View>
+  )
+  renderSeparator = () => (
+    <View
+      style={{
+        height: 1,
+        width: '86%',
+        backgroundColor: '#CED0CE',
+        marginLeft: '14%'
+      }}
+    />
+  )
+  handleRefresh = () => {
+    this.setState({
+      page: 1,
+      refreshing: true,
+      seed: this.state.seed + 1,
+    }, () => {
+      this.getRemoteRequest()
+    })
+  }
+  handleLoadMore = () => {
+    this.setState({
+      page: this.state.page + 1,
+    }, () => {
+      this.getRemoteRequest()
+    })
+  }
+  getKeyExtractor = ({ email }) => email
+  render() {
+    const { data, refreshing } = this.state
+    return (
+      <View>
+        <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+          <FlatList
+            data={data}
+            extraData={this.state}
+            renderItem={this.renderItem}
+            refreshing={refreshing}
+            keyExtractor={this.getKeyExtractor}
+            ItemSeparatorComponent={this.renderSeparator}
+            ListHeaderComponent={this.renderHeader}
+            ListFooterComponent={this.renderFooter}
+            // ListEmptyComponent={this.renderDefault}
+            onRefresh={this.handleRefresh}
+            onEndReached={this.handleLoadMore}
+            onEndReachedThreshold={100}
+          />
+        </List>
       </View>
     )
   }
